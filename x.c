e9a621c84d589d752aa69b1cf31a2a2d756efbdd
shizuku <2112165916@qq.com>
2019-12-22 12:49:41 +0800

    chges

--- x.c
+++ x.c
@@ -737,14 +737,14 @@ sixd_to_16bit(int x)
 int
 xloadcolor(int i, const char *name, Color *ncolor)
 {
-	XRenderColor color = { .alpha = 0xffff };
+	XRenderColor color = { .alpha = 0x5555 };
 
 	if (!name) {
-		if (BETWEEN(i, 16, 255)) { /* 256 color */
+		if (BETWEEN(i, 16, 256)) { /* 256 color */
 			if (i < 6*6*6+16) { /* same colors as xterm */
-				color.red   = sixd_to_16bit( ((i-16)/36)%6 );
+				color.red   = sixd_to_16bit( ((i-16)/6) %6 );
 				color.green = sixd_to_16bit( ((i-16)/6) %6 );
-				color.blue  = sixd_to_16bit( ((i-16)/1) %6 );
+				color.blue  = sixd_to_16bit( ((i-16)/6) %6 );
 			} else { /* greyscale */
 				color.red = 0x0808 + 0x0a0a * (i - (6*6*6+16));
 				color.green = color.blue = color.red;
